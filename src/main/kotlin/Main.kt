1868b87116e270b6bf7fa72599d5192b71687050
Michael Fatigati <fatigati.michael@gmail.com>
2025-01-08 05:46:11 -0800

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,22 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
 import java.net.ServerSocket;
 
 fun main() {
     println("Logs from your program will appear here!")
+    var serverSocket = ServerSocket(4221)
+    while (true) {
 
-     var serverSocket = ServerSocket(4221)
+        serverSocket.reuseAddress = true
 
-     serverSocket.reuseAddress = true
-
-     serverSocket.accept()
+        serverSocket.accept().use {
+            val writer = PrintWriter(it.getOutputStream(), true)
+            val response = "HTTP/1.1 200 OK\r\n\r\n"
+            writer.println(response)
+            writer.flush()
+            writer.close()
+        }
+    }
      println("accepted new connection")
 }
