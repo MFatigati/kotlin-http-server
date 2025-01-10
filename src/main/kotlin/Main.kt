9c3f99153c00bb51d51e0c8ed07b5421936c0b1a
Michael Fatigati <fatigati.michael@gmail.com>
2025-01-10 05:37:55 -0800

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,21 +2,25 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.ServerSocket;
-
 fun main() {
     println("Logs from your program will appear here!")
     var serverSocket = ServerSocket(4221)
     while (true) {
-
         serverSocket.reuseAddress = true
-
         serverSocket.accept().use {
+            val reader = BufferedReader(InputStreamReader(it.getInputStream()))
+            val message = reader.readLine()
+            val path = message.split(" ")[1]
             val writer = PrintWriter(it.getOutputStream(), true)
-            val response = "HTTP/1.1 200 OK\r\n\r\n".toByteArray()
+            var response: String
+            when (path) {
+                "/" -> response = "HTTP/1.1 200 OK\r\n\r\n"
+                else -> response = "HTTP/1.1 404 Not Found\r\n\r\n"
+            }
             writer.println(response)
             writer.flush()
             writer.close()
         }
     }
-     println("accepted new connection")
+    println("accepted new connection")
 }
