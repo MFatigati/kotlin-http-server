6c20ce572a7fe62aff6e10785a1e533b16ef4485
Michael Fatigati <fatigati.michael@gmail.com>
2025-01-16 14:45:28 -0800

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.ServerSocket;
+import java.net.Socket
+import kotlinx.coroutines.*
 
 val echoPathRegex = Regex("/echo/\\w+")
 
@@ -12,13 +14,8 @@ val userAgentHeaderRegex = Regex("User-Agent.*")
 fun getEchoPathString(path: String): String {
     return path.split("/")[2]
 }
-
-fun main() {
-    println("Logs from your program will appear here!")
-    var serverSocket = ServerSocket(4221)
-    while (true) {
-        serverSocket.reuseAddress = true
-        serverSocket.accept().use {
+suspend fun handleRequest(socket: Socket) {
+        socket.use {
             val reader = BufferedReader(InputStreamReader(it.getInputStream()))
             val requestPath = reader.readLine()
             var line: String?
@@ -31,9 +28,10 @@ fun main() {
             if (path == "/") {
                 response = "HTTP/1.1 200 OK\r\n\r\n"
             } else if (echoPathRegex.matches(path)) {
-                val echoString =  getEchoPathString(path)
+                val echoString = getEchoPathString(path)
                 val contentLength = echoString.length
-                response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: $contentLength\r\n\r\n$echoString"
+                response =
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: $contentLength\r\n\r\n$echoString"
             } else if (userAgentPathRegex.matches(path)) {
                 while (true) {
                     line = reader.readLine()
@@ -44,15 +42,25 @@ fun main() {
                     lines.add(line)
                 }
                 val userAgentLength = userAgent.length
-                response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: $userAgentLength\r\n\r\n$userAgent"
+                response =
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: $userAgentLength\r\n\r\n$userAgent"
             } else {
                 response = "HTTP/1.1 404 Not Found\r\n\r\n"
             }
-            
+
             writer.println(response)
             writer.flush()
             writer.close()
         }
+}
+
+fun main() = runBlocking {
+    println("Logs from your program will appear here!")
+    var serverSocket = ServerSocket(4221)
+    while (true) {
+        serverSocket.reuseAddress = true
+        val socket = serverSocket.accept()
+        CoroutineScope(Dispatchers.IO).launch {handleRequest(socket)}
     }
     println("accepted new connection")
 }
